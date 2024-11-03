707c13d42aaa3aa1e606ee9db32bdd122115078a
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2024-11-03 18:34:47 +0100

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pagine/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ChisonoComponent } from './pagine/chisono/chisono.component';
+import { ProgettiComponent } from './pagine/progetti/progetti.component';
+import { ContattiComponent } from './pagine/contatti/contatti.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ChisonoComponent,
+    ProgettiComponent,
+    ContattiComponent
   ],
   imports: [
     BrowserModule,
