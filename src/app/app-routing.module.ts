d3d70eaae833e35abf908c1a19913312d50f1899
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2024-11-13 15:41:58 +0100

    css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pagine/home/home.component';
+import { ContattiComponent } from './pagine/contatti/contatti.component';
+import { ChisonoComponent } from './pagine/chisono/chisono.component';
+import { ProgettiComponent } from './pagine/progetti/progetti.component';
 
 const routes: Routes = [
 
 {path:'home',component:HomeComponent},
 {path:'',component:HomeComponent},
+{path:'contatti',component:ContattiComponent},
+{path:'chisono',component:ChisonoComponent},
+{path:'progetti',component:ProgettiComponent}
 
 
 
