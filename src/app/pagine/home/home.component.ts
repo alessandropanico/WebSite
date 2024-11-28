a2a467fc1476a3339fe867b65c876f0b3cb5fe45
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2024-11-28 17:34:11 +0100

    css

--- src/app/pagine/home/home.component.ts
+++ src/app/pagine/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  ngOnInit(): void {
+    this.initializeVideos();
+  }
+
+  initializeVideos(): void {
+    const videos = document.querySelectorAll('video');
+    videos.forEach(video => {
+      video.load(); // Ricarica il video per forzare il corretto avvio
+      video.play().catch(err => {
+        console.error('Errore nel riprodurre il video:', err);
+      });
+    });
+  }
 }
