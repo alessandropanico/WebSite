a9baeda796f146080b6f7dead18d2ee9a1cde2fa
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2024-12-24 23:25:42 +0100

    css

--- src/app/pagine/home/home.component.ts
+++ src/app/pagine/home/home.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   ngOnInit(): void {
+    // Logica o inizializzazioni generali possono essere messe qui
+  }
+
+  ngAfterViewInit(): void {
     this.initializeVideos();
   }
 
   initializeVideos(): void {
-    const videos = document.querySelectorAll('video');
+    const videos: NodeListOf<HTMLVideoElement> = this.el.nativeElement.querySelectorAll('video');
     videos.forEach(video => {
       video.load(); // Ricarica il video per forzare il corretto avvio
+      video.muted = true; // Mute per evitare restrizioni del browser
       video.play().catch(err => {
-        console.error('Errore nel riprodurre il video:', err);
+        console.warn('Errore nel riprodurre il video:', err);
       });
     });
   }
