ddc46550c057fd57e0c04a822310a0f5125d33ce
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2025-01-21 15:40:28 +0100

    css

--- src/app/pagine/home/home.component.ts
+++ src/app/pagine/home/home.component.ts
@@ -18,9 +18,27 @@ export class HomeComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     this.initializeVideos();
 
-    setTimeout(() => {
-      $('.animate-top').addClass('active');
-    }, 200); // Ritardo di 500ms
+    const elements = document.querySelectorAll('.animate-top');
+
+    // Crea una funzione di callback per l'observer
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        // Aggiungi la classe 'active' solo quando l'elemento è visibile con un offset
+        if (entry.isIntersecting) {
+          entry.target.classList.add('active');
+          observer.unobserve(entry.target); // Interrompe l'osservazione dopo l'attivazione
+        }
+      });
+    }, {
+      root: null, // L'elemento di riferimento è il viewport
+      rootMargin: '0px 0px -100px 0px', // Offset: -100px sul fondo, in modo che l'elemento inizi a essere animato un po' prima di entrare completamente nel viewport
+      threshold: 0.1 // L'elemento deve essere visibile almeno per il 10% (puoi aumentare questo valore se desideri che l'elemento sia più visibile prima di attivare l'animazione)
+    });
+
+    // Osserva ogni elemento con la classe 'animate-top'
+    elements.forEach(element => {
+      observer.observe(element);
+    });
   }
 
   initializeVideos(): void {
